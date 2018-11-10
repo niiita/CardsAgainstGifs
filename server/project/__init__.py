591812cc12f07460ba572284fcab5de70c57b181
Amarisse <amarissemarie23@gmail.com>
2018-11-10 15:27:33 -0500

    adding api

--- server/project/__init__.py
+++ server/project/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
+GIPHY_STORE = {}
 
 def create_app(script_info=None):
     # instantiate the app
@@ -9,9 +10,26 @@ def create_app(script_info=None):
     app = Flask(__name__)
     CORS(app)
     setup_blueprints(app)
+    
+    call_api()
+
+
     return app
 
 
 def setup_blueprints(app):
     from project.routes.views import views_bp
     app.register_blueprint(views_bp)
+
+
+def call_api():
+    import requests
+    giphy_key = os.getenv('GIPHY_API_KEY')
+    limit = 50
+    url = f'http://api.giphy.com/v1/gifs/trending?api_key={giphy_key}&limit={limit}'
+
+    response = requests.get(url).json()
+
+    for gif in response["data"]:
+        GIPHY_STORE[gif['id']] = gif['embed_url']
+
